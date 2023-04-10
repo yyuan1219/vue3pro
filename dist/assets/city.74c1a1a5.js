8c8095dd889bfd388b4b343e34ee95fefa451be3
13687598695 <365100916@qq,com>
2023-04-10 11:54:28 +0800

    修改页面

---
+++ dist/assets/city.74c1a1a5.js
@@ -0,0 +1,1 @@
+import{h as e}from"./index.9f046415.js";import{a8 as i}from"./index.e03d35ab.js";function a(){return e.get({url:"/city/all"})}const l=i("city",{state:()=>({allCities:{},currentCity:{cityName:"\u5E7F\u5DDE"}}),actions:{async fetchAllCitiesData(){const t=await a();this.allCities=t.data}}});export{l as u};
